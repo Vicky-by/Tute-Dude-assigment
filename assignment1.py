55880995c305cbd0c2e4a02c849584feaee30659
Vicky-by <vikki5603@gmail.com>
2025-09-23 12:32:14 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,26 @@
+# Q. write a program 
+#task two number as input from user 
+# performs the besic  mathmatical operation ,addition,subtraction,multiplication,division
+
+num1=int(input("first number:"))
+num2=int(input("second number:"))
+
+print("first number=" ,num1,"second number=",num2)
+
+add=num1+num2
+sub=num1 - num2
+mult=num1* num2
+div=num1/num2
+
+print("Addition=",add)
+print("Subtraction=",sub)
+print("Multiplication=",mult)
+print("Division=",div)
+
+#task2  
+first=input("first name: ")
+last=input("last name: ")
+
+fullname=first+last+"!"
+print("hello,",fullname,"!welcome to the python program.")
+
