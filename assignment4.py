bb1a2f30f7e57a5a8f2ed3e3cf89e652832b5349
Vicky-by <vikki5603@gmail.com>
2025-09-25 13:59:12 +0530

    Update assignment4.py

--- assignment4.py
+++ assignment4.py
@@ -7,8 +7,6 @@ try:
 except FileNotFoundError:
     print("The file 'sample.txt' was not found.")
 
-
-
 #----Task2---------------
 # Step 1: Take user input and write to output.txt
 # Step 1: Take user input and write to output.txt
@@ -23,7 +21,8 @@ with open("output.txt", "a") :
     file.write(additional_input + "\n")
 
 # # Step 3: Read and display the final content of the file
-# print("\nFinal content of output.txt:")
-# with open("output.txt", "r") :
-#     content = file.read()
-#     print(content)
+print("\nFinal content of output.txt:")
+with open("output.txt", "r") :
+    content = file.read()
+    print(content)
+
